4c44b2fdc18ca4eb69967e57a5502d88440f35ff
nomena <vahatra.nomena@yahoo.com>
2023-12-08 21:29:47 +0300

    c

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+cp target/demo-1.0-SNAPSHOT.war /Applications/apache-tomcat-10/webapps
---
+++ src/test/java/ClasseTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class ClasseTest {
+    @Test
+    void test() {
+        System.out.println("Hello World!");
+    }
+}
