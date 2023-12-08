3c65aba000b94b4bf646b386f0ab5549fbacb648
nomena <vahatra.nomena@yahoo.com>
2023-12-08 23:21:19 +0300

    Update HelloServlet.java

--- src/main/java/org/example/demo/HelloServlet.java
+++ src/main/java/org/example/demo/HelloServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-
     public void init() {
         message = "Hello World!";
     }
